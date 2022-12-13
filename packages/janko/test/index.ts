ad2a93491c51101eb5819429896ead6b66ce2f62
nickvoloshyn <tryvols@gmail.com>
2022-12-13 04:31:14 +0200

    Remove bot token from test

--- packages/janko/test/index.ts
+++ packages/janko/test/index.ts
@@ -3,7 +3,7 @@ import { TestController } from "./test-controller";
 
 function bootstrap() {
     const app = new TelegramBotApplication({
-        token: "5684351457:AAGFrOOkcxo02ZH79r5HNVmTQnp-naG8CTs",
+        token: "some-token",
         polling: true
     });
     app.registerController(TestController);
